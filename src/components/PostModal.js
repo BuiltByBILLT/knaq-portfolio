cd50407bdbe68fefdfa0a4b94e02b0332e621d16
Brandon <brandon@builtbybillt.com>
2022-04-25 20:05:44 -0500

    Resized PostModal to fit on screen

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Modal } from 'react-bootstrap'
 import Post from './Post'
 
+
 const PostModal = ({ show, setShow, post }) => {
     const handleClose = () => setShow(false);
 
     return (
-        <Modal show={show} onHide={handleClose} centered size="lg">
+        <Modal show={show} onHide={handleClose} centered size="lg" dialogClassName='postModal'>
             <Modal.Body>
                 <Post post={post} />
             </Modal.Body>
