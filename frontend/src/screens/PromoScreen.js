ce005700a3eca3372bbd01987e8804d479d2211e
Brandon <brandon@builtbybillt.com>
2022-09-08 11:00:40 -0500

    updated text on PromoScreen

--- frontend/src/screens/PromoScreen.js
+++ frontend/src/screens/PromoScreen.js
@@ -12,8 +12,9 @@ const PromoScreen = () => {
                 </Col>
                 <Col lg={6} className="my-auto">
                     <div>
-                        <h1 className="mt-5 mt-lg-0" style={{ fontSize: "36px", fontWeight: "500", lineHeight: "1.2" }}>Monetize your content like never before. No minimums or thresholds. Start earning now!</h1>
-                        <h2 className="my-5" style={{ fontSize: "24px", fontWeight: "500", lineHeight: "1.8" }} >Creators put a lot of effort into their work. It is time to reward them properly. We help Creators connect with fans while earning to their fullest potential. </h2>
+                        <h1 className="mt-5 mt-lg-0" style={{ fontSize: "36px", fontWeight: "500", lineHeight: "1.2" }}>Knaq: Aligning Creators and their Communities</h1>
+                        <h2 className="my-5" style={{ fontSize: "24px", fontWeight: "500", lineHeight: "1.8" }} >For Creators: Connect and rewaed your most loyal fanbase </h2>
+                        <h2 className="my-5" style={{ fontSize: "24px", fontWeight: "500", lineHeight: "1.8" }} >For Fans: Interact-To-Earn exclusive experiences with your favorite Creators </h2>
                         <div className="mb-5  d-lg-flex align-items-center justify-content-center text-center ">
                             <p className="my-0 mr-5">Try our open beta:</p>
                             <a href='https://apps.apple.com/us/app/knaq/id1490253270' target="_blank">
