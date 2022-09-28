9d02a0af28f8bdda6a26e08f4865afecb93a6037
Brandon <brandon@builtbybillt.com>
2022-09-28 15:47:14 -0700

    mobile friendly

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -4,12 +4,15 @@ import StaticPage from '../components/StaticPage'
 import { useMutation } from 'react-query'
 import axios from 'axios'
 import Logo from '../components/Logo'
+import useMediaQuery from '../hooks/useMediaQuery'
 
 const LandingScreen = () => {
 
     const [email, setEmail] = useState("")
     const [success, setSuccess] = useState(false)
     const [error, setError] = useState(false)
+
+    const isLarge = useMediaQuery("(min-width: 992px)")
 
     const { mutate: waitlistHandler, isLoading } = useMutation((e) => {
         e.preventDefault()
@@ -21,6 +24,7 @@ const LandingScreen = () => {
 
     return (
         <>
+            {false && <div style={{ position: "fixed", top: 0, zIndex: 20 }}>Is Large: {isLarge.toString()}</div>}
             <div id="hero" style={{ backgroundImage: "url('images/BG1.jpg')", backgroundSize: "100% 100%", backgroundRepeat: "no-repeat" }}>
                 {/* <Navbar> */}
 
@@ -31,9 +35,9 @@ const LandingScreen = () => {
                     <Nav.Link className="px-1 mt-3 mr-3" active={false} target="_blank" href="http://www.twitter.com/knaqapp"><img className="social-icons" src="./icons/twitter_icon.png" /></Nav.Link>
                 </Nav>
                 {/* </Navbar> */}
-                <Container className="pb-5">
-                    <Row className='py-5 px-4'>
-                        <Col lg={{ span: 10, offset: 1 }} className="my-auto">
+                <Container id="waitlist" className="pb-5">
+                    <Row className='py-5 px-2 px-lg-4'>
+                        <Col xs={12} lg={{ span: 10, offset: 1 }} className="my-auto">
                             <div>
                                 <h1 className="mt-5 mx-auto" style={{ textAlign: "center", color: "var(--indigo)", maxWidth: "750px" }}>Creating a deeper connection between fans and creators</h1>
                                 <p className="my-4" style={{ textAlign: "center", fontSize: "20px" }}>Earn tokens when you engage with your favorite creators </p>
@@ -43,12 +47,18 @@ const LandingScreen = () => {
                                     : <Form inline onSubmit={waitlistHandler} className="justify-content-center">
                                         <Form.Control placeholder="type your email" type="email"
                                             value={email} onChange={(e) => setEmail(e.target.value)}
-                                            style={{ borderRadius: "100px", width: "350px", height: "40px" }}
-                                            className="px-4 mr-2 indigoForm"
+                                            style={{
+                                                borderRadius: "100px", width: "350px", height: "40px",
+                                                textAlign: isLarge ? "left" : "center"
+                                            }}
+                                            className="px-4 mr-lg-2 indigoForm"
                                         />
                                         <Button variant='secondary' type="submit"
-                                            style={{ borderRadius: "100px", height: "40px" }}
-                                            className="pl-4 pr-2 py-0"
+                                            style={{
+                                                borderRadius: "100px", height: "40px",
+                                                width: isLarge ? "" : "350px"
+                                            }}
+                                            className="pl-4 pr-2 py-0 mt-lg-0 mt-2"
                                         >
                                             join waitlist
                                             <img src="/icons/arrow_right_1.svg" style={{ height: "70%" }} className="pl-3" />
@@ -60,33 +70,36 @@ const LandingScreen = () => {
                     </Row>
                 </Container>
 
-                <Row className="mx-0 my-5" style={{ background: "linear-gradient(to right, #392269, #bf1a2f, #f68721, #39b54a, #007abb)" }}>
+                <Row id="rainbowbar" className="mx-0 my-5" style={{ background: "linear-gradient(to right, #392269, #bf1a2f, #f68721, #39b54a, #007abb)" }}>
                     <Container>
-                        <Row className="" style={{}}>
-                            <Col className="my-3 d-flex align-items-center justify-content-end">
-                                <h6 style={{ color: "var(--tan)", }}>total users</h6>
+                        <Row className="" style={{ color: "var(--tan)" }}>
+                            <Col xs={12} lg={4} className="my-3 d-flex align-items-center justify-content-center">
+                                <h6>total users</h6>
                                 <img style={{ height: "35px" }} className="svg-icon mx-3" src="icons/users-icon.svg" />
-                                <h4 style={{ color: "var(--tan)", }}>494,117</h4>
+                                <h4>494,117</h4>
                             </Col>
-                            <Col className="my-3 d-flex align-items-center justify-content-center"
-                                style={{ borderLeft: "2px solid var(--tan)", borderRight: "2px solid var(--tan)" }} >
-                                <h6 style={{ color: "var(--tan)", }}>payouts earned</h6>
+                            <Col xs={12} lg={4} className="my-3 d-flex align-items-center justify-content-center"
+                                style={{
+                                    borderLeft: isLarge ? "2px solid var(--tan)" : "",
+                                    borderRight: isLarge ? "2px solid var(--tan)" : ""
+                                }} >
+                                <h6>payouts earned</h6>
                                 <img style={{ height: "35px" }} className="svg-icon mx-3" src="icons/money-icon.svg" />
-                                <h4 style={{ color: "var(--tan)", }}>131,050</h4>
+                                <h4>131,050</h4>
                             </Col>
-                            <Col className="my-3 d-flex align-items-center justify-content-start">
-                                <h6 style={{ color: "var(--tan)", }}>loyalty earned</h6>
+                            <Col xs={12} lg={4} className="my-3 d-flex align-items-center justify-content-center">
+                                <h6>loyalty earned</h6>
                                 <img style={{ height: "35px" }} className="svg-icon mx-3" src="icons/heart-icon.svg" />
-                                <h4 style={{ color: "var(--tan)", }}>120,210</h4>
+                                <h4>120,210</h4>
                             </Col>
                         </Row>
                     </Container>
                 </Row>
 
-                <Container className='pb-5 px-0' style={{ color: "var(--tan)", position: "relative" }}>
+                <Container id="blue" className='pb-5 px-0' style={{ color: "var(--tan)", position: "relative" }}>
                     <>
-                        <div className='pb-5' style={{ position: "absolute", width: "100%", height: "100%", zIndex: 10 }}>
-                            <Row style={{ height: "100%" }}>
+                        {isLarge && <div id="arrow layer" className='pb-5' style={{ position: "absolute", width: "100%", height: "100%", zIndex: 10 }}>
+                            <Row className="mx-0" style={{ height: "100%" }}>
                                 <Col className="px-0" style={{ position: "relative", height: "100%" }}>
                                 </Col>
                                 <Col className="px-0" style={{ position: "relative", height: "100%" }}>
@@ -96,29 +109,29 @@ const LandingScreen = () => {
                                     <img src="./icons/arrow_right_1.svg" style={{ width: "72px", position: 'absolute', left: "-36px", top: "calc(50% - 36px)" }} />
                                 </Col>
                             </Row>
-                        </div>
-                        <Row className='pb-5'>
-                            <Col style={{}}>
+                        </div>}
+                        <Row id="rombus layer" className='pb-5 mx-0'>
+                            <Col xs={12} lg={4}>
                                 <div className="rombus rombus-left rombus-girl">
-                                    <div style={{ height: "100%" }} className="pb-5 px-5 rombus-right d-flex flex-column align-items-center justify-content-center" >
-                                        <h1 className="mb-3 px-2" style={{ fontSize: "34px" }}>Interact & Earn Coins </h1>
-                                        <p className="px-2" style={{ fontSize: "18px" }}>Interact with your favorite creators to earn tokens</p>
+                                    <div className="pb-5 px-5 rombus-right d-flex flex-column align-items-center justify-content-center" >
+                                        <h1 className="mb-3 px-2">Interact & Earn Coins </h1>
+                                        <p className="px-2">Interact with your favorite creators to earn tokens</p>
                                     </div>
                                 </div>
                             </Col>
-                            <Col style={{}}>
+                            <Col xs={12} lg={4}>
                                 <div className="rombus rombus-right rombus-tokens">
-                                    <div style={{ height: "100%" }} className="pb-5 px-5 rombus-left d-flex flex-column align-items-center justify-content-center">
-                                        <h1 className="mb-3 px-2" style={{ fontSize: "34px" }}>Get Tiered Benefits</h1>
-                                        <p className="px-2" style={{ fontSize: "18px" }}>Accumulate tokens for tiered benefits</p>
+                                    <div className="pb-5 px-5 rombus-left d-flex flex-column align-items-center justify-content-center">
+                                        <h1 className="mb-3 px-2">Get Tiered Benefits</h1>
+                                        <p className="px-2">Accumulate tokens for tiered benefits</p>
                                     </div>
                                 </div>
                             </Col>
-                            <Col style={{}}>
+                            <Col xs={12} lg={4}>
                                 <div className="rombus rombus-left rombus-meetup">
-                                    <div style={{ height: "100%" }} className="pb-5 px-5 rombus-right d-flex flex-column align-items-center justify-content-center">
-                                        <h1 className="mb-3 px-2" style={{ fontSize: "34px" }}>Redeem for Exclusives</h1>
-                                        <p className="px-2" style={{ fontSize: "18px" }}>Redeem tokens for exclusive experiences and merch</p>
+                                    <div className="pb-5 px-5 rombus-right d-flex flex-column align-items-center justify-content-center">
+                                        <h1 className="mb-3 px-2">Redeem for Exclusives</h1>
+                                        <p className="px-2">Redeem tokens for exclusive experiences and merch</p>
                                     </div>
                                 </div>
                             </Col>
@@ -141,6 +154,46 @@ const LandingScreen = () => {
                 </Container>
             </div>
 
+            {false && <div id="quad">
+                <Container>
+                    {/* <div style={{ background: "linear-gradient(45deg, #f7ece1, #cac4ce)", width: "100%", height: "400px" }} /> */}
+                    <Row id="rombus layer" className='mx-0'>
+                        <Col className="p-1">
+                            <div className="rombus rombus-quad" style={{ background: "linear-gradient(45deg, #f7ece1 0%, #cac4ce 25%)" }}>
+                                <div className="pb-5 rombus-quad-inner d-flex flex-column align-items-center justify-content-center" >
+                                    <h1 className="mb-3 px-2">Interact & Earn Coins </h1>
+                                    <p className="px-2">Interact with your favorite creators to earn tokens</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="p-1">
+                            <div className="rombus rombus-quad" style={{ background: "linear-gradient(45deg, #f7ece1 25%, #cac4ce 50%)" }}>
+                                <div className="pb-5 rombus-quad-inner d-flex flex-column align-items-center justify-content-center">
+                                    <h1 className="mb-3 px-2">Get Tiered Benefits</h1>
+                                    <p className="px-2">Accumulate tokens for tiered benefits</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="p-1">
+                            <div className="rombus rombus-quad" style={{ background: "linear-gradient(45deg, #f7ece1 50%, #cac4ce 75%)" }}>
+                                <div className="pb-5 rombus-quad-inner d-flex flex-column align-items-center justify-content-center">
+                                    <h1 className="mb-3 px-2">Redeem for Exclusives</h1>
+                                    <p className="px-2">Redeem tokens for exclusive experiences and merch</p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="p-1">
+                            <div className="rombus rombus-quad" style={{ background: "linear-gradient(45deg, #f7ece1 75%, #cac4ce 100%)" }}>
+                                <div className="pb-5 rombus-quad-inner d-flex flex-column align-items-center justify-content-center">
+                                    <h1 className="mb-3 px-2">Redeem for Exclusives</h1>
+                                    <p className="px-2">Redeem tokens for exclusive experiences and merch</p>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>}
+
             <div id="join">
                 <Container className="py-5">
                     <Row className='py-5 px-4'>
@@ -160,17 +213,22 @@ const LandingScreen = () => {
 
 
             <Navbar bg="dark" variant="dark" style={{}} className="py-5">
-                <Nav>
-                    <Nav.Link className="ml-3" active={false} target="_blank" href="/faq">FAQs</Nav.Link>
-                    <Nav.Link className="ml-3" active={false} target="_blank" href="/terms">Terms</Nav.Link>
-                    <Nav.Link className="ml-3" active={false} target="_blank" href="/privacy">Privacy</Nav.Link>
-
-                </Nav>
-                <Nav className="ml-auto ">
-                    <Nav.Link className="px-1" active={false} target="_blank" href="https://discord.gg/H4TAHRGNHR"><img className="social-icons" src="./icons/discord_icon.png" /></Nav.Link>
-                    <Nav.Link className="px-1 " active={false} target="_blank" href="http://www.instagram.com/knaqapp"><img className="social-icons" src="./icons/instagram_icon.png" /></Nav.Link>
-                    <Nav.Link className="px-1 mr-3" active={false} target="_blank" href="http://www.twitter.com/knaqapp"><img className="social-icons" src="./icons/twitter_icon.png" /></Nav.Link>
-                </Nav>
+                <Row style={{ width: "100%" }} className="mx-0">
+                    <Col xs={12} lg="auto">
+                        <Nav className={isLarge ? "" : "flex-column"}>
+                            <Nav.Link className="mx-lg-2" active={false} target="_blank" href="/faq">FAQs</Nav.Link>
+                            <Nav.Link className="mx-lg-2" active={false} target="_blank" href="/terms">Terms</Nav.Link>
+                            <Nav.Link className="mx-lg-2" active={false} target="_blank" href="/privacy">Privacy</Nav.Link>
+                        </Nav>
+                    </Col>
+                    <Col xs={12} lg="auto" className="ml-auto">
+                        <Nav className="">
+                            <Nav.Link className="px-1" active={false} target="_blank" href="https://discord.gg/H4TAHRGNHR"><img className="social-icons" src="./icons/discord_icon.png" /></Nav.Link>
+                            <Nav.Link className="px-1 " active={false} target="_blank" href="http://www.instagram.com/knaqapp"><img className="social-icons" src="./icons/instagram_icon.png" /></Nav.Link>
+                            <Nav.Link className="px-1 mr-3" active={false} target="_blank" href="http://www.twitter.com/knaqapp"><img className="social-icons" src="./icons/twitter_icon.png" /></Nav.Link>
+                        </Nav>
+                    </Col>
+                </Row>
             </Navbar>
         </>
     )
